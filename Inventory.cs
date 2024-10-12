184745ed15233edbbab063ba962e93ff8f540f65
Developer <developer@example.com>
2024-10-12 07:14:38 +0000

    Initial commit: Inventory class

---
+++ Inventory.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace ECommerceSystem
+{
+    public class Inventory
+    {
+        private List<string> items = new List<string>();
+
+        public void AddItem(string item)
+        {
+            items.Add(item);
+            Console.WriteLine($"Added item to inventory: {item}");
+        }
+
+        public void RemoveItem(string item)
+        {
+            items.Remove(item);
+            Console.WriteLine($"Removed item from inventory: {item}");
+        }
+
+        public void ShowInventory()
+        {
+            Console.WriteLine("Inventory items:");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
